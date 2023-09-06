6bc409a80c996573077aa578af1cfd273a32e29e
Mehul Mohan <mehulmpt@gmail.com>
2023-09-06 17:01:08 +0530

    Add files via script

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<h1>{{ msg }}</h1>
+
+		<button @click="count++">count is: {{ count }}</button>
+	</div>
+</template>
+
+<script>
+export default {
+	props: {
+		msg: String,
+	},
+	data() {
+		return {
+			count: 0,
+		};
+	},
+};
+</script>
+
+<style scoped>
+a {
+	color: #42b983;
+}
+</style>
